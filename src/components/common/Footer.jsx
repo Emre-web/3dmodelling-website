370c0c98faa411ac675c915a6f0cafb451cf2efc
Emre Yıldırım <Emreyildirimbro@gmail.com>
2023-01-13 16:11:06 +0300

    3dmodelling-webProject

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Copyright © YILDIRIM SOFTWARE all rights reserved. Powered by EMRE YILDIRIM.</p>
+      </footer>
+    </>
+  )
+}
